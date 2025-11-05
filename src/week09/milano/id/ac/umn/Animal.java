d9bf67c764f1a4fb2915550741b972e4b40d1e3f
Milano0505 <milanopandey345@gmail.com>
2025-11-05 10:55:53 +0700

    Interface Animal

---
+++ src/week09/milano/id/ac/umn/Animal.java
@@ -0,0 +1,7 @@
+package week09.milano.id.ac.umn;
+
+public interface Animal {
+    public void eat();
+    public void travel();
+}
+
---
+++ src/week09/milano/id/ac/umn/Mammal.java
@@ -0,0 +1,16 @@
+package week09.milano.id.ac.umn;
+
+public class Mammal implements Animal {
+    public Mammal() {}
+    public void eat() {
+        System.out.println("Mammal eats");
+    }
+    public void travel() {
+        System.out.println("Mammal travel");
+    }
+    public static void main(String[] args) {
+        Mammal m = new Mammal();
+        m.eat();
+        m.travel();
+    };
+}
